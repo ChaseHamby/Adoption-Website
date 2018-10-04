17679137e7b969deeaf3dcc6f5c27a4337186ce5
Chase Hamby <c.chase.hamby@gmail.com>
2018-10-03 20:49:59 -0500

    working on JSON

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,23 @@
-console.log('hi');
+import {pets, petsBuilder} from './components/petComponent.js'
+
+const sortPets = (e) => {
+    const type = e.target.id;
+    if (type === 'Cats') {
+        petsBuilder(pets);
+    // } else {
+    //     const filteredPets = pets.filter(x => x.type === type);
+    //     petsBuilder(filteredPets);
+    // }
+    }
+};
+
+const sortEvents = () => {
+    const dogButton = document.getElementById('Dogs');
+    const catButton = document.getElementById('Cats');
+    const dinoButton = document.getElementById('Dinos');
+    dogButton.addEventListener('click', sortPets);
+    catButton.addEventListener('click', sortPets);
+    dinoButton.addEventListener('click', sortPets);
+}
+
+export {sortEvents}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,11 @@
+// import {getPets} from './data/petData.js'
 import {petsBuilder} from './components/petComponent.js'
+import {sortEvents} from './events.js'
 
 const initializeApp = () => {
     petsBuilder();
+    sortEvents();
+    // getPets();
  };
     
 initializeApp();
