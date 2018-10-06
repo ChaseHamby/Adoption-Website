33975d5293755646e923609e7c83d932c9d4a7d1
Chase Hamby <c.chase.hamby@gmail.com>
2018-10-06 10:07:55 -0500

    finished sorting function and everything works

--- javascripts/events.js
+++ javascripts/events.js
@@ -2,22 +2,25 @@ import {pets, petsBuilder} from './components/petComponent.js'
 
 const sortPets = (e) => {
     const type = e.target.id;
-    if (type === 'Cats') {
+    if (type === 'all') {
         petsBuilder(pets);
-    // } else {
-    //     const filteredPets = pets.filter(x => x.type === type);
-    //     petsBuilder(filteredPets);
-    // }
+        console.log('if it works?');
+    } else {
+        const filteredPets = pets.filter(x => x.type === type);
+        petsBuilder(filteredPets);
+        console.log('else it works?');
     }
 };
 
 const sortEvents = () => {
-    const dogButton = document.getElementById('Dogs');
-    const catButton = document.getElementById('Cats');
-    const dinoButton = document.getElementById('Dinos');
+    const allButton = document.getElementById('all');
+    const dogButton = document.getElementById('dog');
+    const catButton = document.getElementById('cat');
+    const dinoButton = document.getElementById('dino');
+    allButton.addEventListener('click', sortPets);
     dogButton.addEventListener('click', sortPets);
     catButton.addEventListener('click', sortPets);
     dinoButton.addEventListener('click', sortPets);
-}
+};
 
 export {sortEvents}
